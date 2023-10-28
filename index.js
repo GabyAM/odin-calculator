f4b8c95bc6ab1104d453c9283f809415546396ec
GabyAM <gdgabyam@gmail.com>
2023-10-28 12:55:50 -0300

    adds chained operations and clear button
    the user can now chain multiple operations. There is also a new clear (CE) button which sets the calculator to it's initial value.

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ function operate (numA, numB, operator) {
     }
 }
 
+function getResultFromOperation (operation) {
+    const values = operation.split(' ')
+    if(Number(values[2]) === 0 && values[1] === '%') return 'NOOOOOOO'
+    return operate(Number(values[0]), Number(values[2]), values[1]).toString()
+}
+
 function display(input) {
     const $display = document.querySelector('.display')
     $display.textContent = input
@@ -21,26 +27,33 @@ function init() {
     const $numberButtons = document.querySelectorAll('.calc-button.number');
     $numberButtons.forEach(button => button.onclick = () => {
         operation += button.textContent
-        console.log(`se actualiza operation a ${operation}`)
         lastInput = button.textContent
         display(operation)
     })
     const $operatorButtons = document.querySelectorAll('.calc-button.operator');
     $operatorButtons.forEach(button => button.onclick = () => {
         if (!isNaN(lastInput)) {
+            if(operation.includes(' ')) {
+                operation = getResultFromOperation(operation)
+            }
             operation += button.textContent
-            console.log(`se actualiza operation a ${operation}`)
             lastInput = button.textContent 
+            display(operation)
         }
     })
     const $equalButton = document.querySelector('.calc-button.equal');
     $equalButton.onclick = () => {
         if(Number(lastInput) !== NaN){
-            console.log(operation)
-            const values = operation.split(' ')
-            const result = operate(Number(values[0]), Number(values[2]), values[1])
+            const result = getResultFromOperation(operation);
             display(result)
+            if(result === 'NOOOOOOO') operation = ''
         }
+    }
+    const $clearButton = document.querySelector('.calc-button.clear');
+    $clearButton.onclick = () => {
+        operation = '';
+        lastInput = '';
+        display('');
     }
 }
 
